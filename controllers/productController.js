f74bd65b65436902b8c879b60c28fc30da0e1011
davi <davijluna@gmail.com>
2022-08-16 17:24:02 -0300

    feat: arquivo controllers

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,27 +1,36 @@
 const productServices = require('../services/productServices');
 
-const getAll = async (_req, res, next) => {
-  try {
-    const resultado = await productServices.getAll();
-    res.status(200)
-      .json(resultado);
-  } catch (erro) {
-    next('error');
-  }
+const getAll = async (_req, res) => {
+  const { data, error, code } = await productServices.getAll();
+  if (error) return res.status(code).json(error);
+  return res.status(code).json(data);
 };
 
-const getId = async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const resultado = await productServices.getId(id);
-    if (!resultado) {
-      return res.status(404)
-        .json({ message: 'Product not found' });
-  }
-   res.status(200).json(resultado);
-  } catch (erro) {
-    next('error');
-  }
+const getId = async (req, res) => {
+  const { data, error, code } = await productServices.getId(req.params);
+  if (error) return res.status(code).json(error);
+  return res.status(code).json(data);
 };
 
-module.exports = { getAll, getId };
+const NewProducts = async (req, res) => {
+  const { data, error, code } = await productServices.NewProducts(req.body);
+  if (error) return res.status(code).json(error);
+  return res.status(code).json(data);
+};
+
+// const { id } = req.params;
+//   const resultado = await productServices.getId(id);
+//   if (!resultado) {
+//     return res.status(404)
+//       .json({ message: 'Product not found' });
+// }
+//  res.status(200).json(resultado);
+// } catch (erro) {
+//   next('error');
+// }
+//   if (!name) {
+//     return { code: 400, message: { message: '"name" not definit' } };
+//   }
+// const result = await productServices.NewProducts(name);
+// return { code: 201, message: result[0] };
+module.exports = { getAll, getId, NewProducts };
